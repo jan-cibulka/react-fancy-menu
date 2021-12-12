9ca898132cb7495237769a2ecaa84816c5c5fe56
jan.cibulka <jan.cibulka@hotmail.cz>
2021-12-12 22:35:14 +0100

    to react

--- react-fancy-menu/src/App.tsx
+++ react-fancy-menu/src/App.tsx
@@ -1,12 +1,41 @@
 import "./App.css";
-import Menu from "./menu/Menu";
+import Menu, { MenuItem } from "./menu/Menu";
+import {
+  IoHomeOutline,
+  IoPersonOutline,
+  IoChatbubblesOutline,
+  IoCameraOutline,
+  IoSettingsOutline,
+} from "react-icons/io5";
 
 function App() {
   return (
     <div className='App'>
-      <Menu />
+      <Menu menuData={menuData} />
     </div>
   );
 }
 
+const menuData: MenuItem[] = [
+  {
+    name: "Home",
+    icon: <IoHomeOutline />,
+  },
+  {
+    name: "Profile",
+    icon: <IoPersonOutline />,
+  },
+  {
+    name: "Message",
+    icon: <IoChatbubblesOutline />,
+  },
+  {
+    name: "Photos",
+    icon: <IoCameraOutline />,
+  },
+  {
+    name: "Settings",
+    icon: <IoSettingsOutline />,
+  },
+];
 export default App;
--- react-fancy-menu/src/menu/Menu.tsx
+++ react-fancy-menu/src/menu/Menu.tsx
@@ -1,83 +1,40 @@
-import React, { useCallback, useEffect, VoidFunctionComponent } from "react";
+import React, { useCallback, useState, VoidFunctionComponent } from "react";
 import "./Menu.css";
 
-import {
-  IoHomeOutline,
-  IoPersonOutline,
-  IoChatbubblesOutline,
-  IoCameraOutline,
-  IoSettingsOutline,
-} from "react-icons/io5";
-interface MenuProps {}
+export interface MenuItem {
+  name: string;
+  icon: JSX.Element;
+}
 
-const Menu: VoidFunctionComponent<MenuProps> = () => {
-  const list = document.querySelectorAll(".list");
+interface MenuProps {
+  menuData: MenuItem[];
+}
 
-  const handleMenuClick = useCallback(
-    (ev: Event) => {
-      list.forEach((item) => {
-        item.classList.remove("active");
-      });
-      console.log(ev.target as HTMLElement);
-      (ev.target as HTMLElement).parentElement?.classList.add(
-        "active"
-      );
-    },
-    [list]
-  );
+const Menu: VoidFunctionComponent<MenuProps> = ({ menuData }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  useEffect(() => {
-    console.log("init");
-    list.forEach((item) => item.addEventListener("click", handleMenuClick));
-  }, [handleMenuClick, list]);
+  const handleItemClick = useCallback((i: number) => {
+    setActiveIndex(i);
+  }, []);
 
   return (
     <div className='root'>
       <div className='navigation'>
         <ul>
-          <li className='list active'>
-            <a href='#'>
-              <span className='icon'>
-                <IoHomeOutline />
-              </span>
-              <span className='text'>Home</span>
-            </a>
-          </li>
+          {menuData.map((item, index) => (
+            <li
+              className={`item ${index === activeIndex && "active"}`}
+              key={index}
+              onClick={() => {
+                handleItemClick(index);
+              }}>
+              <a href='#'>
+                <span className='icon'>{item.icon}</span>
+                <span className='text'>{item.name}</span>
+              </a>
+            </li>
+          ))}
 
-          <li className='list'>
-            <a href='#'>
-              <span className='icon'>
-                <IoPersonOutline />
-              </span>
-              <span className='text'>Profile</span>
-            </a>
-          </li>
-
-          <li className='list'>
-            <a href='#'>
-              <span className='icon'>
-                <IoChatbubblesOutline />
-              </span>
-              <span className='text'>Message</span>
-            </a>
-          </li>
-          <li className='list'>
-            <a href='#'>
-              <span className='icon'>
-                <IoCameraOutline />
-              </span>
-              <span className='text'>Photos</span>
-            </a>
-          </li>
-
-          <li className='list'>
-            <a href='#'>
-              <span className='icon'>
-                <IoSettingsOutline />
-              </span>
-              <span className='text'>Settings</span>
-            </a>
-          </li>
           <div className='indicator'></div>
         </ul>
       </div>
