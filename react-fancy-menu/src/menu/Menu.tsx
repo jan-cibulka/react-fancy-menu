e7f40dea14f682c75113f923a279d4ad37bdd502
jan.cibulka <jan.cibulka@hotmail.cz>
2021-12-12 20:10:54 +0100

    add icons

---
+++ react-fancy-menu/src/menu/Menu.tsx
@@ -0,0 +1,67 @@
+import React, { VoidFunctionComponent } from "react";
+import "./Menu.css";
+
+import {
+  IoHomeOutline,
+  IoPersonOutline,
+  IoChatbubblesOutline,
+  IoCameraOutline,
+  IoSettingsOutline,
+} from "react-icons/io5";
+interface MenuProps {}
+
+const Menu: VoidFunctionComponent<MenuProps> = () => {
+  return (
+    <div className='root'>
+      <div className='navigation'>
+        <ul>
+          <li className='list'>
+            <a href='/'>
+              <span className='icon'>
+                <IoHomeOutline />
+              </span>
+              <span className='text'>Home</span>
+            </a>
+          </li>
+
+          <li className='list'>
+            <a href='/'>
+              <span className='icon'>
+                <IoPersonOutline />
+              </span>
+              <span className='text'>Profile</span>
+            </a>
+          </li>
+
+          <li className='list'>
+            <a href='/'>
+              <span className='icon'>
+                <IoChatbubblesOutline />
+              </span>
+              <span className='text'>Message</span>
+            </a>
+          </li>
+          <li className='list'>
+            <a href='/'>
+              <span className='icon'>
+                <IoCameraOutline />
+              </span>
+              <span className='text'>Photos</span>
+            </a>
+          </li>
+
+          <li className='list'>
+            <a href='/'>
+              <span className='icon'>
+                <IoSettingsOutline />
+              </span>
+              <span className='text'>Settings</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
